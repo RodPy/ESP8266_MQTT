7871bb371d446bd8fd1eed6b57e68e76b5506205
Rodney <rodneyrrspy@gmail.com>
2024-08-28 09:25:42 -0400

    Create MQTT.ino

---
+++ MQTT/MQTT.ino
@@ -0,0 +1,93 @@
+#include "Wire.h"
+
+#include "math.h"
+
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+#include <WiFi.h>
+
+int lastState = HIGH; 
+int currentState;   
+
+#define WLAN_SSID   "WiFiCJP-Alumnos"
+#define WLAN_PASS   "cjpalumnos2021"
+
+#define HOST        "10.100.0.151"
+
+#define PORT        1883
+#define USERNAME    "linux"
+#define PASSWORD    "linux"
+
+//time out loop count
+const int timeout = 200;
+
+WiFiClient client;
+Adafruit_MQTT_Client mqtt(&client, HOST, PORT, USERNAME, PASSWORD);
+Adafruit_MQTT_Publish acelera_x = Adafruit_MQTT_Publish(&mqtt, "acelerometro/ax");
+Adafruit_MQTT_Publish acelera_y = Adafruit_MQTT_Publish(&mqtt, "acelerometro/ay");
+Adafruit_MQTT_Publish acelera_z = Adafruit_MQTT_Publish(&mqtt, "acelerometro/az");
+Adafruit_MQTT_Publish button = Adafruit_MQTT_Publish(&mqtt, "acelerometro/button");
+
+
+void MQTT_connect();
+
+void setup() { 
+  delay(1); 
+  WiFi.mode(WIFI_STA);  
+  Serial.begin(115200);
+  Serial.print("Connecting to ");
+  Serial.println(WLAN_SSID);
+
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  int i = 0;
+  for (; i < timeout; i++)
+  {
+    if(WiFi.status() == WL_CONNECTED) break;
+    delay(100);
+    Serial.print(".");
+  }
+  if(i == timeout)
+    Serial.println("No Conectado");
+  LedConect(100, red);
+
+  Serial.println("IP address: "); Serial.println(WiFi.localIP());
+  LedConect(100, green);
+
+  MQTT_connect();
+  init_MPU_sensor();
+}
+  void loop() 
+{
+
+  // acelera_x.publish(ax_d);
+  // delay(1);
+  // acelera_y.publish(ay_d);
+  // delay(1);
+  // acelera_z.publish(az_d);
+
+
+}
+
+void MQTT_connect() {
+  int8_t ret;
+  if (mqtt.connected()) {
+    return;
+  }
+
+  Serial.print("Connecting to MQTT... ");
+  uint8_t retries = 10;
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Serial.println(mqtt.connectErrorString(ret));
+       Serial.println("Retrying MQTT connection in 1 second...");
+       mqtt.disconnect();
+       delay(1000);
+       retries--;
+       if (retries == 0)
+         Serial.println("No Conectado");
+           LedConect(200, red);
+
+  }
+  Serial.println("MQTT Connected!");
+   LedConect(100, blue);
+}
+
